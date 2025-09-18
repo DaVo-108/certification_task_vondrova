c2d424060e2c13cf222e6b1ed8bc9e67331d037c
Daniela Vondrová <daniela.jurasikova108@gmail.com>
2025-09-18 18:09:40 +0200

    Nastavení 2

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      tegb_login_url: "https://tegb-frontend-88542200c6db.herokuapp.com/",
+      tegb_registr_url:
+        "https://tegb-frontend-88542200c6db.herokuapp.com/register",
+    },
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
   },
